b51dc7fb800bef2f57f660f98f34b4113f8b9b1a
Стас Басов <stas.basov@whatever.com>
2023-09-16 22:26:20 +0300

    Annonate functions and variables

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice = None
-    char_class = None
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input(
             'Введи название персонажа, за которого хочешь играть: '
@@ -80,3 +80,15 @@ def choice_char_class() -> str:
             'или любую другую кнопку, '
             'чтобы выбрать другого персонажа ').lower()
     return char_class
+
+
+def main():
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
